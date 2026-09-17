1e7ef870f4dd4822412c1928b265f3c6af646dd5
Yuvarani R <yuvaraniramesh27@gmail.com>
2026-09-17 20:15:04 +0530

    Fix Vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,4 +6,3 @@ export default defineConfig({
   plugins: [react()],
   base: '/student-registration-system/',
 })
-
